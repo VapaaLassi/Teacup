9d5f61073f4b72b4c70a530cc03d9463864e1a7b
VapaaLassi <vapaalassi@gmail.com>
2023-02-04 15:38:06 +0200

    Player animation controller finished.

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,17 @@ public class Movement : MonoBehaviour
 {
     public AnimationCurve movementCurve;
 
+    public Animator animator;
+
+    public SpriteRenderer spriteRenderer;
+
+    public enum Direction { Up, Down, Left, Right };
+
+    private Direction currentDirection = Direction.Down;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     public float speed = 5;
@@ -21,16 +29,15 @@ public class Movement : MonoBehaviour
     void Update()
     {
 
-
-
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
 
         float curveMultiplier = 1;
 
         //print("x: " + x + " y: " + y);
+        SetAnimationState(x, y);
 
-        if(Mathf.Abs(x) > 0 || Mathf.Abs(y) > 0)
+        if (Mathf.Abs(x) > 0 || Mathf.Abs(y) > 0)
         {
             if (!moving)
             {
@@ -61,7 +68,66 @@ public class Movement : MonoBehaviour
         }
 
         //print(speedMultiplier);
+        animator.SetBool("Moving", moving);
 
         transform.Translate(x * speedMultiplier, y * speedMultiplier,0);
+    }
+
+    private void SetAnimationState(float x, float y)
+    {
+        if (Math.Abs(x) > Mathf.Abs(y))
+        {
+            if (x > 0)
+            {
+                UpdateDirection(Direction.Right, "Right");
+                return;
+            }
+            else if (x < 0)
+            {
+                UpdateDirection(Direction.Left, "Left");
+
+                return;
+            }
+        }
+        else
+        {
+            if (y > 0)
+            {
+                UpdateDirection(Direction.Up, "Up");
+
+                return;
+            }
+            else if (y < 0)
+            {
+                UpdateDirection(Direction.Down, "Down");
+
+                return;
+            }
+
+        }
+    }
+
+    private void UpdateDirection(Direction direction, string trigger)
+    {
+        if(direction == currentDirection && moving)
+        {
+            return;
+        }
+        currentDirection = direction;
+        animator.SetTrigger(trigger);
+        if (direction == Direction.Left)
+        {
+            spriteRenderer.flipX = true;
+        } else
+        {
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName("WalkLeft") || animator.GetCurrentAnimatorStateInfo(0).IsName("IdleLeft"))
+            {
+
+            } else
+            {
+                spriteRenderer.flipX = false;
+            }
+        }
+        return;
     }
 }
