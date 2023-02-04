01efd35a9819d3391786075449068d07ebe9d472
VapaaLassi <vapaalassi@gmail.com>
2023-02-04 23:25:42 +0200

    Added melody system for dead ends.

--- Assets/LowpassBasedOnDistance.cs
+++ Assets/LowpassBasedOnDistance.cs
@@ -11,15 +11,29 @@ public class LowpassBasedOnDistance : MonoBehaviour
     public AnimationCurve lowpassCurve;
     public AnimationCurve distortionCurve;
 
+    public AnimationCurve melodyCurve; 
+
+    private AudioSource mySource;
+
+    public AudioSource melody;
+
+    private float maxdistance = 25f;
+
     // Start is called before the first frame update
     void Start()
     {
+        mySource = GetComponent<AudioSource>();
+        maxdistance = mySource.maxDistance;
     }
 
     // Update is called once per frame
     void Update()
     {
         float distance = Vector2.Distance(transform.position, target.position);
+
+        // Could also fade out the piano layer here if that exists.
+
+        melody.volume = melodyCurve.Evaluate(1 - distance / maxdistance) / 2;
         //print(distance);
         //mixer.SetFloat("Lowpass", lowpassCurve.Evaluate(distance));
         mixer.SetFloat("DistortionLevel", distortionCurve.Evaluate(distance));
