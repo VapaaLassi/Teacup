f686f2b4fcd1735e6b1d51af68bf42f431cf5920
VapaaLassi <vapaalassi@gmail.com>
2023-02-06 14:37:11 +0200

    Added animation for getting overwhelmed.

--- Assets/CopyPlayerSprites.cs
+++ Assets/CopyPlayerSprites.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class CopyPlayerSprites : MonoBehaviour
 {
     public SpriteRenderer playerSpriteRenderer;
+    private SpriteRenderer mySpriteRenderer;
 
     void Start()
     {
-        
+        mySpriteRenderer = GetComponent<SpriteRenderer>();  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        mySpriteRenderer.sprite = playerSpriteRenderer.sprite;
+        mySpriteRenderer.flipX = !playerSpriteRenderer.flipX;
     }
 }
