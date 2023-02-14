a8cac6808888d68dabb74263bafa12062c070452
VapaaLassi <vapaalassi@gmail.com>
2023-02-14 16:42:07 +0200

    Removed unused import

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
-using UnityEngine.WSA;
 using Application = UnityEngine.Application;
 
 public class GameManager : MonoBehaviour
